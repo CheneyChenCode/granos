c3a10a5380ba52b3fe93ff5f1a160ede73ad9348
cheney <chiayui.chen@gmail.com>
2024-09-09 15:57:44 +0800

    test logback config in properties

--- src/main/java/com/grace/granos/controller/Home.java
+++ src/main/java/com/grace/granos/controller/Home.java
@@ -1,5 +1,7 @@
 package com.grace.granos.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +11,12 @@ import com.grace.granos.service.StaffService;
 
 @Controller
 public class Home {
+	private static final Logger logger = LoggerFactory.getLogger(Home.class);
 	@Autowired
 	StaffService staffService;
 	@RequestMapping("/home")
 	public String home(Model model){
-		
+		logger.info("welcome!!!");
 		return "home";    
 	}
 }
